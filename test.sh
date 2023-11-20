f6873c10ac76fd7d3c82372cbbf6181f94f20af0
[KAZUKI] <[kzkdrmlove@gmail.com]>
2023-11-20 06:28:01 +0000

    step1

--- test.sh
+++ test.sh
@@ -1,10 +1,14 @@
+#!/bin/bash
+#どのshebang（インタプリタ）で実行するかを指定する
+
 echo "パスワードマネージャーへようこそ！"
 
 read -p "サービス名を入力してください：" service
 read -p "ユーザー名を入力してください：" user
 read -p "パスワードを入力してください：" password
 
-if [$service $user $password];then
+if [ "$service" ] && [ "$user" ] && [ "$password" ]; then
+	echo   "サービス名：$service ユーザー名：$user パスワード：$password" >> pass.txt  
 	echo "Thanks"
 else
 	echo "入力箇所に誤りがあります"
