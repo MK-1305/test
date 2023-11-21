7d8b3296279b2f4b185e49695cafea4de6185d1f
[KAZUKI] <[kzkdrmlove@gmail.com]>
2023-11-21 03:52:03 +0000

    入力文字がcommandとして認識される

--- test.sh
+++ test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #どのshebang（インタプリタ）で実行するかを指定する
 
-echo "パスワードマネージャーへようこそ！"
-read -p "次の選択肢から入力してください(Add Password/Get Password/Exit)：" choice
+while true; do
 
-if $choice == "Add Password"; then
+ echo "パスワードマネージャーへようこそ！"
+ read -p "次の選択肢から入力してください(Add Password/Get Password/Exit)：" choice
+
+ if [ "$choice" == "Add Password" ]; then
 
 	read -p "サービス名を入力してください：" service
 	read -p "ユーザー名を入力してください：" user
@@ -12,8 +14,29 @@ if $choice == "Add Password"; then
 
 	if [ "$service" ] && [ "$user" ] && [ "$password" ]; then
 		echo   "サービス名：$service ユーザー名：$user パスワード：$password" >> pass.txt  
-		echo "Thanks"
+		echo "Thank you!"
 	else
-		echo "入力箇所に誤りがあります"
-	fi	
-fi	
+		echo "すべて入力してください"
+	fi
+
+
+	elif [ "$choice" == "Get Password" ]; then
+	
+		read -p "サービス名を入力してください：" service
+	
+		grep [ "$service" ] pass.txt
+	
+		echo "サービス名：$service"
+		echo "ユーザー名：$user"
+		echo "パスワード：$password"
+
+
+	elif  [ "$choice" == "Exit" ]; then
+		echo "Thank you!"
+		break
+
+	else
+		echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
+ fi
+
+done
